b2c9de160ffe74cce7e1f0f513d698e532e8687b
Victor Compertino <gifpa45we@gmail.com>
2026-01-17 12:35:06 -0300

    feat: add entity NinjaModel to the database

--- CadastroDeNinjas/src/main/java/com/victorvzx/CadastroDeNinjas/NinjaModel.java
+++ CadastroDeNinjas/src/main/java/com/victorvzx/CadastroDeNinjas/NinjaModel.java
@@ -1,5 +1,56 @@
 package com.victorvzx.CadastroDeNinjas;
 
+
+import jakarta.persistence.*;
+
+
+// JPA = Java Persistence API
+
+// Entity transforma uma classe numa entidade do banco de dados
+@Entity
+// Cria a tabela "tb_cadastro_de_ninjas"
+@Table(name = "tb_cadastro")
 public class NinjaModel {
 
+    @Id
+    // Define como o ID será gerado, nesse caso, estratégia IDENTITY!
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
