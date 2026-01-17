b6670732dc5853b19b574c14bb4b691ad14bfede
Victor Compertino <gifpa45we@gmail.com>
2026-01-17 10:58:44 -0300

    feat: add routing to the project

---
+++ CadastroDeNinjas/src/main/java/com/victorvzx/CadastroDeNinjas/Controller.java
@@ -0,0 +1,21 @@
+package com.victorvzx.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "Pagina inicial";
+    }
+
+    @GetMapping("/boasvindas")
+    public String boasVindas() {
+        return "Essa é a minha primeira mensagem nessa rota";
+    }
+
+}
